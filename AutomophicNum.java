7ec40d8d35050b5d9d219b3241c903318a943827
Prathamesh Farade <159513648+prathu1846@users.noreply.github.com>
2024-03-20 17:56:11 +0530

    Add files via upload

---
+++ AutomophicNum.java
@@ -0,0 +1,23 @@
+package day100;
+
+public class AutomophicNum {
+
+	public static void main(String[] args) {
+		
+		int n=10;
+		int y=n*n;
+	
+		if(y%10== n%10) {
+			
+			System.out.println("It is a Automophic Number");			
+		} else
+			
+			System.out.println("It is a NOT Autophoric Number");
+	}
+
+}
+
+//Check Whether Or Not the Number is an Automorphic Number in Java.
+//Given an integer input, the Objective is to check whether the square of the 
+//number ends with the same number or not. 
+//Therefore, we’ll write a code to Check Whether or Not the Number is an Automorphic Number in Java Language.
---
+++ PerfectNUm.java
@@ -0,0 +1,26 @@
+package day100;
+
+public class PerfectNUm {
+
+	public static void main(String[] args) {
+		
+		
+		int n=6 ,sum=0;
+		
+		for(int i=1; i<n; i++) {
+			
+			if(n%i==0) {
+				
+				sum=sum+i;
+				
+			}
+		}
+		if(sum==n) 
+			System.out.println(n+   "It is Perfect Number");
+			else 
+				System.out.println(n+  "It is Not Perfect Number");
+			
+				
+	}
+
+}
